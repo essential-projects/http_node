c63e385c7f78bc6aa334071b2e33d0b04fb10529
Heiko Mathes <heiko.mathes@gmail.com>
2017-09-08 17:24:03 +0200

    :fire: remove dist-folder

--- dist/commonjs/index.js
+++
@@ -1,9 +0,0 @@
-"use strict";
-function __export(m) {
-    for (var p in m) if (!exports.hasOwnProperty(p)) exports[p] = m[p];
-}
-Object.defineProperty(exports, "__esModule", { value: true });
-__export(require("./base_router"));
-__export(require("./http_extension"));
-
-//# sourceMappingURL=index.js.map
