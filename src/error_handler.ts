1d75fa6e171f44b41370be0373d362093af2b84f
Heiko Mathes <heiko.mathes@gmail.com>
2018-01-23 14:42:08 +0100

    :sparkles: add error-handling

---
+++ src/error_handler.ts
@@ -0,0 +1,15 @@
+import {BaseError as EssentialProjectsError} from '@essential-projects/errors';
+import {NextFunction, Request, Response} from 'express';
+
+function errorIsEssentialProjectsError(error: any): error is EssentialProjectsError {
+  return error.isEssentialProjectsError === true;
+}
+
+export function errorHandler(error: Error | EssentialProjectsError, request: Request, response: Response, next: NextFunction): void {
+  let statusCode: number = 500;
+  if (errorIsEssentialProjectsError(error)) {
+    statusCode = error.code;
+  }
+
+  response.status(statusCode).send(error.message);
+}
