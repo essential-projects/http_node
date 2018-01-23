3001b7f3589c0bfae4355ff9f1d5bef3705ba1d0
Heiko Mathes <heiko.mathes@gmail.com>
2018-01-23 17:41:51 +0100

    :truck: move isError to errors-package

--- src/error_handler.ts
+++ src/error_handler.ts
@@ -1,14 +1,10 @@
-import {BaseError as EssentialProjectsError, ErrorCodes} from '@essential-projects/errors.ts';
+import {BaseError as EssentialProjectsError, ErrorCodes, isError as errorIsEssentialProjectsError} from '@essential-projects/errors.ts';
 import {NextFunction, Request, Response} from 'express';
 import {Logger} from 'loggerhythm';
 
 const logger: Logger = Logger
                         .createLogger('http_node')
                         .createChildLogger('error_handler');
-
-function errorIsEssentialProjectsError(error: any): error is EssentialProjectsError {
-  return error.isEssentialProjectsError === true;
-}
 
 export function errorHandler(error: Error | EssentialProjectsError, request: Request, response: Response, next: NextFunction): void {
   let statusCode: number = ErrorCodes.InternalServerError;
