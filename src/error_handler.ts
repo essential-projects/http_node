032994a4f313ee436758a66598b7db529e907a98
Heiko Mathes <heiko.mathes@gmail.com>
2018-01-23 16:36:51 +0100

    :recycle: improve non-error handling

--- src/error_handler.ts
+++ src/error_handler.ts
@@ -1,12 +1,23 @@
-import {BaseError as EssentialProjectsError} from '@essential-projects/errors.ts';
+import {BaseError as EssentialProjectsError, ErrorCodes} from '@essential-projects/errors.ts';
 import {NextFunction, Request, Response} from 'express';
+import {Logger} from 'loggerhythm';
+
+const logger: Logger = Logger.createLogger('http_node').createChildLogger('error_handler');
 
 function errorIsEssentialProjectsError(error: any): error is EssentialProjectsError {
   return error.isEssentialProjectsError === true;
 }
 
 export function errorHandler(error: Error | EssentialProjectsError, request: Request, response: Response, next: NextFunction): void {
-  let statusCode: number = 500;
+  let statusCode: number = ErrorCodes.InternalServerError;
+  let responseMessage: string = '';
+
+  if (error instanceof Error) {
+    responseMessage = error.message;
+  } else {
+    logger.warn('non-error thrown:', error);
+  }
+
   if (errorIsEssentialProjectsError(error)) {
     statusCode = error.code;
   }
