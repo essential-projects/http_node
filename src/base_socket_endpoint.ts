a1d139872c46f3a73552aa37f33bca353ec181ff
5ebastianMeier <sebastian.meier@5minds.de>
2018-09-12 14:36:07 +0200

    remove unused variables

--- src/base_socket_endpoint.ts
+++ src/base_socket_endpoint.ts
@@ -2,13 +2,6 @@ import {IHttpSocketEndpoint} from '@essential-projects/http_contracts';
 
 export class BaseSocketEndpoint implements IHttpSocketEndpoint {
 
-  private _socketServer: SocketIO.Server = undefined;
-
-  public config: any = undefined;
-
-  /* tslint:disable-next-line:no-empty */
-  constructor() { }
-
   public initializeEndpoint(socketIo: SocketIO.Namespace): Promise<any> | any { return; }
 
   public dispose(): Promise<void> | void { return; }
